bd4b6614621f934f88b20210652f9b8796658816
Sukanya-Banerjee <144331238+Sukanya-Banerjee@users.noreply.github.com>
2023-12-29 16:46:04 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,37 @@
+package org.anudip.mavenApplication.mapquestions;
+
+import java.util.HashMap;
+
+	public class Student
+	{
+		 public static void main(String[] args) 
+		 {
+		      
+			 // Sample arrays of student IDs and marks
+		        String[] studentIds = {"s1", "s2", "s3", "s4","s5"};
+		        int[] studentMarks = {96,90, 85, 95, 88};
+
+		        // Create a HashMap to store student information
+		        HashMap<String, Integer> studentMap = new HashMap<>();
+
+		        // Iterate through the arrays and store information in the HashMap
+		        for (int i = 0; i < studentIds.length; i++) {
+		            String studentId = studentIds[i];
+		            int studentMark = studentMarks[i];
+
+		            // Store the information in the HashMap
+		            studentMap.put(studentId, studentMark);
+		        }
+
+		        // Print the stored values in the HashMap
+		        System.out.println("Student Information:");
+		        for (String id : studentMap.keySet()) {
+		            int mark = studentMap.get(id);
+		            System.out.println("Student ID: " + id + ", Marks: " + mark);
+		        }
+
+		        // Print the size of the HashMap
+		        System.out.println("Size of the HashMap: " + studentMap.size());
+		    }
+		
+}
